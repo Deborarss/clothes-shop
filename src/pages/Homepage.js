8531152ff5f11b5f51590074f5695a526ac23686
Debora <debora_rss@hotmail.com>
2020-11-28 16:10:52 -0300

    Add style to homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,9 +2,35 @@ import * as React from "react";
 import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
-  menu: {},
-  item: {},
-  content: {},
+  menu: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    padding: "20px 80px",
+    width: "100%",
+  },
+  item: {
+    alignItems: "center",
+    border: "1px solid",
+    display: "flex",
+    flex: "auto",
+    height: "240px",
+    justifyContent: "center",
+    margin: "0 8px 16px",
+    minWidth: "30%",
+  },
+  content: {
+    border: "1px solid",
+    padding: "25px",
+    textAlign: "center",
+  },
+  title: {
+    color: "#4a4a4a",
+    fontSize: "22px",
+  },
+  subtitle: {
+    fontSize: "16px",
+  },
 });
 
 const Homepage = () => {
@@ -14,36 +40,36 @@ const Homepage = () => {
     <div className={classes.menu}>
       <div className={classes.item}>
         <div className={classes.content}>
-          <h2>Bonés</h2>
-          <span>Compre agora!</span>
+          <h2 className={classes.title}>Bonés</h2>
+          <span className={classes.subtitle}>Compre agora!</span>
         </div>
       </div>
 
       <div className={classes.item}>
         <div className={classes.content}>
-          <h2>Jaquetas</h2>
-          <span>Compre agora!</span>
+          <h2 className={classes.title}>Jaquetas</h2>
+          <span className={classes.subtitle}>Compre agora!</span>
         </div>
       </div>
 
       <div className={classes.item}>
         <div className={classes.content}>
-          <h2>Tênis</h2>
-          <span>Compre agora!</span>
+          <h2 className={classes.title}>Tênis</h2>
+          <span className={classes.subtitle}>Compre agora!</span>
         </div>
       </div>
 
       <div className={classes.item}>
         <div className={classes.content}>
-          <h2>Mulheres</h2>
-          <span>Compre agora!</span>
+          <h2 className={classes.title}>Mulheres</h2>
+          <span className={classes.subtitle}>Compre agora!</span>
         </div>
       </div>
 
       <div className={classes.item}>
         <div className={classes.content}>
-          <h2>Homens</h2>
-          <span>Compre agora!</span>
+          <h2 className={classes.title}>Homens</h2>
+          <span className={classes.subtitle}>Compre agora!</span>
         </div>
       </div>
     </div>
